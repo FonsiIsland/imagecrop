e4158648747372652112ef8ed3b0e07bb3c48b6b
FonsiIsland <57556044+FonsiIsland@users.noreply.github.com>
2024-11-26 12:41:43 +0100

    First Test

--- source.js
+++ source.js
@@ -1,8 +1,14 @@
 var img;
 var input;
 
-var move = false;
-var posX = 0, posY = 0;
+let posX = 200; // Startposition X
+let posY = 200; // Startposition Y
+let rectWidth = 200;
+let rectHeight = 300;
+let move = false;
+let offsetX = 0;
+let offsetY = 0;
+
 
 //height of our horizontal slice
 var sliceHeight = 10;
@@ -15,22 +21,14 @@ function preload() {
 }
 
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight);
+    var canvas = createCanvas(0, 0);
     canvas.parent('sketch-holder');
     
-  
-  
-   input = createFileInput(handleImage);
-  input.position(0, 500);
-  
-  
-  console.log(img)
- 
+    input = createFileInput(handleImage);
+    input.position(0, 500);
  
     //let croped = img.get(100, 100, img.width-100, img.height-100);
-    
-    //image(croped, 100,100);
-  
+    //image(croped, 100,100);  
 }
 
 function draw() {
@@ -39,31 +37,63 @@ function draw() {
   // Draw the image if loaded.
   if (img) {
     
-    if(move) {
+
+    if(img.width > 750){
+        resizeCanvas(750, img.height / img.width * 750);
+        //image(img, posX, posY, img.width, img.height);
+
+        image(img, 0, 0, 1*width, 1 *img.height*width/img.width); 
+
+        //img.resize(1000, 0);
+    }
+
+    
+       // Koordinaten des sichtbaren Rechtecks
+    let rectX = posX-(rectWidth/2) // X-Position des Rechtecks
+    let rectY = posY-(rectHeight/2); // Y-Position des Rechtecks
+   
+
+    // Zeichne das graue Overlay, aber spare das Rechteck aus
+    noStroke();
+    fill(50, 50, 50, 150); // Dunkelgrau mit Transparenz
+    rect(0, 0, width, rectY); // Oberhalb des sichtbaren Bereichs
+    rect(0, rectY, rectX, rectHeight); // Links vom sichtbaren Bereich
+    rect(rectX + rectWidth, rectY, width - (rectX + rectWidth), rectHeight); // Rechts vom sichtbaren Bereich
+    rect(0, rectY + rectHeight, width, height - (rectY + rectHeight)); // Unterhalb des sichtbaren Bereichs
+
+    if (move) {
+      // Neue Position basierend auf der Maus
       posX = mouseX;
       posY = mouseY;
-    }
     
-    image(img, posX, posY, img.width, img.height);
+      // Grenzen festlegen, damit das Rechteck nicht aus dem Canvas herausgeschoben wird
+      posX = constrain(mouseX - offsetX, (rectWidth/2), width - (rectWidth/2)); // Begrenzung auf Canvas-Breite
+      posY = constrain(mouseY - offsetY, (rectHeight/2), height - (rectHeight/2)); // Begrenzung auf Canvas-Höhe
+    }
   }
 }
 
 
 function mousePressed() {
-  
-  move = true;
+  if (
+    mouseX > posX - rectWidth / 2 &&
+    mouseX < posX + rectWidth / 2 &&
+    mouseY > posY - rectHeight / 2 &&
+    mouseY < posY + rectHeight / 2
+  ) {
+    move = true;
+
+    // Berechne den Offset zwischen der Maus und der Rechteckposition
+    offsetX = mouseX - posX;
+    offsetY = mouseY - posY;
+  }
 }
 
 
 function mouseReleased() {
-  
   move = false;
-  
-  
   //saveCanvas('myCanvas.jpg');
 }
-
-
 
 
 function handleImage(file) {
@@ -72,5 +102,14 @@ function handleImage(file) {
     img.hide();
   } else {
     img = null;
+  }
+}
+
+function keyPressed() {
+  if (key === 's') {
+    // Ausschnitt des Rechtecks erfassen
+    let img = get(posX - rectWidth / 2, posY - rectHeight / 2, rectWidth, rectHeight);
+    // Bild speichern
+    save(img, 'rect_snapshot.png'); // Der Nutzer lädt das Bild herunter
   }
 }
