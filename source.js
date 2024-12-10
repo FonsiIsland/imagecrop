7c64eb240e983820cc5d85faecf6d8d10fd1d10d
FonsiIsland <57556044+FonsiIsland@users.noreply.github.com>
2024-12-10 11:27:16 +0100

    Beta 1.9

--- source.js
+++ source.js
@@ -4,7 +4,7 @@ var input;
 
 // https://codepen.io/DanielHarty/details/vRRxxL
 
-let cropMaskRatios = [[16,9],[20,10],[10,10],[36,11],[27,6],[8,10]]
+let cropMaskRatios = [[16, 9], [9, 16], [20, 10], [10, 10], [36, 11], [27, 6], [8, 10]]
 let selectedRatioIndex = 0;
 let maskScale = 1;
 let maskScaleStop = false;
@@ -17,8 +17,12 @@ let posY = 200; // Startposition Y
 let rectWidth = 10;
 let rectHeight = 10;
 let move = false;
+let resizeGrab = { top: false, bottom: false, left: false, right: false, startPos: undefined, scale: 1 }
 let offsetX = 0;
 let offsetY = 0;
+
+let offsetResizeX = 0;
+let offsetResizeY = 0;
 
 let imageLoading = true;
 
@@ -44,31 +48,31 @@ function preload() {
 }
 
 function setup() {
-    var canvas = createCanvas(0, 0);
-    canvas.parent('sketch-holder');
-    
-    input = createFileInput(handleImage);
-    input.elt.style.display='none';
- 
-    //input = document.createElement('input');
-    //input.type = 'file';
-    //input.style.display='none';
-    
-    // input.onchange = e => { 
-    //    var file = e.target.files[0]; 
-    //    handleImage(file);
-    // }
+  var canvas = createCanvas(0, 0);
+  canvas.parent('sketch-holder');
 
-    document.getElementById("zoomScaleAdd").disabled = true;
-    document.getElementById("zoomScaleRemove").disabled = true;
-    document.getElementById("filedownload").disabled = true;
-    document.querySelectorAll('.mdc-radio__native-control').forEach((radio) => radio.disabled = true);
-    document.querySelectorAll('.mdc-checkbox__native-control').forEach((radio) => radio.disabled = true);
+  input = createFileInput(handleImage);
+  input.elt.style.display = 'none';
 
-    //let croped = img.get(100, 100, img.width-100, img.height-100);
-    //image(croped, 100,100);  
+  //input = document.createElement('input');
+  //input.type = 'file';
+  //input.style.display='none';
 
-    updateOverallScale();
+  // input.onchange = e => { 
+  //    var file = e.target.files[0]; 
+  //    handleImage(file);
+  // }
+
+  document.getElementById("zoomScaleAdd").disabled = true;
+  document.getElementById("zoomScaleRemove").disabled = true;
+  document.getElementById("filedownload").disabled = true;
+  document.querySelectorAll('.mdc-radio__native-control').forEach((radio) => radio.disabled = true);
+  document.querySelectorAll('.mdc-checkbox__native-control').forEach((radio) => radio.disabled = true);
+
+  //let croped = img.get(100, 100, img.width-100, img.height-100);
+  //image(croped, 100,100);  
+
+  updateOverallScale();
 
 }
 
@@ -85,46 +89,46 @@ function draw() {
   if (originalImg && !imageLoading) {
 
     mimicedImg = originalImg;
-    
+
 
     //if(mimicedImg.width > canvasWidth){
-      //resizeCanvas(img.width,img.height)
-        resizeCanvas(canvasWidth, mimicedImg.height / mimicedImg.width * canvasWidth);
-        //image(img, 0, 0, img.width, img.height);
+    //resizeCanvas(img.width,img.height)
+    resizeCanvas(canvasWidth, mimicedImg.height / mimicedImg.width * canvasWidth);
+    //image(img, 0, 0, img.width, img.height);
 
-        image(mimicedImg, 0, 0, 1*width, 1 *mimicedImg.height*width/mimicedImg.width); 
+    image(mimicedImg, 0, 0, 1 * width, 1 * mimicedImg.height * width / mimicedImg.width);
 
-        //img.resize(1000, 0);
+    //img.resize(1000, 0);
     //}
 
 
-    
-    rectWidth = 10* cropMaskRatios[selectedRatioIndex][0] * Math.round((zoomScale*maskScale) *100)/100;
-    rectHeight = 10*cropMaskRatios[selectedRatioIndex][1] * Math.round((zoomScale*maskScale) *100)/100;
 
-    
-    if(rectWidth >= width) {
+    rectWidth = 10 * cropMaskRatios[selectedRatioIndex][0] * Math.round((zoomScale * maskScale * resizeGrab.scale) * 100) / 100;
+    rectHeight = 10 * cropMaskRatios[selectedRatioIndex][1] * Math.round((zoomScale * maskScale * resizeGrab.scale) * 100) / 100;
+
+
+    if (rectWidth >= width) {
       rectWidth = width;
       rectHeight = rectWidth / cropMaskRatios[selectedRatioIndex][0] * cropMaskRatios[selectedRatioIndex][1];
 
       maskScaleStop = true;
     }
 
-    if(rectHeight >= height) {
+    if (rectHeight >= height) {
       rectHeight = height;
       rectWidth = rectHeight / cropMaskRatios[selectedRatioIndex][1] * cropMaskRatios[selectedRatioIndex][0];
 
       maskScaleStop = true;
     }
 
-    if(rectWidth < width && rectHeight < height)
+    if (rectWidth < width && rectHeight < height)
       maskScaleStop = false;
 
 
-       // Koordinaten des sichtbaren Rechtecks
-    let rectX = Math.round(posX-(rectWidth/2)) // X-Position des Rechtecks
-    let rectY = Math.round(posY-(rectHeight/2)) // Y-Position des Rechtecks
-   
+    // Koordinaten des sichtbaren Rechtecks
+    let rectX = Math.round(posX - (rectWidth / 2)) // X-Position des Rechtecks
+    let rectY = Math.round(posY - (rectHeight / 2)) // Y-Position des Rechtecks
+
     //console.log("x: " + rectX + " y: " +rectY + " | w: " + rectWidth +" h: " + rectHeight)
 
     // Zeichne das graue Overlay, aber spare das Rechteck aus
@@ -136,50 +140,55 @@ function draw() {
     rect(0, rectY + rectHeight, width, height - (rectY + rectHeight)); // Unterhalb des sichtbaren Bereichs
 
     fill(color('#cb99c9')); // Dunkelgrau mit Transparenz
-    rect(rectX, rectY-2, rectWidth, 2); // Oben
-    rect(rectX, rectY+rectHeight, rectWidth, 2); // Unten
-    
-    rect(rectX-2, rectY-2, 2, rectHeight + 4); // Links
-    rect(rectX+rectWidth, rectY-2, 2, rectHeight +4); // Rechts
+    rect(rectX, rectY - 2, rectWidth, 2); // Oben
+    rect(rectX, rectY + rectHeight, rectWidth, 2); // Unten
 
-    
+    rect(rectX - 2, rectY - 2, 2, rectHeight + 4); // Links
+    rect(rectX + rectWidth, rectY - 2, 2, rectHeight + 4); // Rechts
+
+
     cursor('default')
 
-    if(mouseX > rectX && mouseX < rectX+rectWidth && mouseY > rectY && mouseY < rectY+rectHeight) {
+    if (mouseX > rectX && mouseX < rectX + rectWidth && mouseY > rectY && mouseY < rectY + rectHeight) {
       cursor('grab')
-    }else if(mouseX > rectX && mouseX < rectX+rectWidth) {
-      if(mouseY > rectY-2 && mouseY < rectY){
+    } else if (mouseX > rectX && mouseX < rectX + rectWidth) {
+      if (mouseY > rectY - 2 && mouseY < rectY) {
         cursor('row-resize');
-      }else if(mouseY > rectY+rectHeight && mouseY < rectY+rectHeight+2){
+      } else if (mouseY > rectY + rectHeight && mouseY < rectY + rectHeight + 2) {
         cursor('row-resize');
       }
-    }else if(mouseY > rectY && mouseY < rectY+rectHeight) {
-      if(mouseX > rectX-2 && mouseX < rectX){
+    } else if (mouseY > rectY && mouseY < rectY + rectHeight) {
+      if (mouseX > rectX - 2 && mouseX < rectX) {
         cursor('col-resize');
-      }else if(mouseX > rectX+rectWidth && mouseX < rectX+rectWidth+2){
+      } else if (mouseX > rectX + rectWidth && mouseX < rectX + rectWidth + 2) {
         cursor('col-resize');
       }
     }
 
+    // if (resizeGrab.top) {
+    //   console.log("Diff: " + (mouseY - resizeGrab.startPos));
+
+    // }
+
     if (move) {
-    
+
       // Grenzen festlegen, damit das Rechteck nicht aus dem Canvas herausgeschoben wird
-      posX = constrain(mouseX - offsetX, (rectWidth/2), width - (rectWidth/2)); // Begrenzung auf Canvas-Breite
-      posY = constrain(mouseY - offsetY, (rectHeight/2), height - (rectHeight/2)); // Begrenzung auf Canvas-Höhe
-    }else{
-      posX = constrain(posX, (rectWidth/2), width - (rectWidth/2)); // Begrenzung auf Canvas-Breite
-      posY = constrain(posY, (rectHeight/2), height - (rectHeight/2)); // Begrenzung auf Canvas-Höhe
+      posX = constrain(mouseX - offsetX, (rectWidth / 2), width - (rectWidth / 2)); // Begrenzung auf Canvas-Breite
+      posY = constrain(mouseY - offsetY, (rectHeight / 2), height - (rectHeight / 2)); // Begrenzung auf Canvas-Höhe
+    } else {
+      posX = constrain(posX, (rectWidth / 2), width - (rectWidth / 2)); // Begrenzung auf Canvas-Breite
+      posY = constrain(posY, (rectHeight / 2), height - (rectHeight / 2)); // Begrenzung auf Canvas-Höhe
     }
   }
 }
 
-const updateOverallScale = () => { 
+const updateOverallScale = () => {
   let oldCavnasWidth = canvasWidth;
-  canvasWidth = (windowWidth / 1920 * defaulCanvasWidth)*zoomScale;
+  canvasWidth = (windowWidth / 1920 * defaulCanvasWidth) * zoomScale;
 
   posX = posX / oldCavnasWidth * canvasWidth;
 
-  if(mimicedImg)
+  if (mimicedImg)
     posY = posY / (mimicedImg.height / mimicedImg.width * oldCavnasWidth) * (mimicedImg.height / mimicedImg.width * canvasWidth);
 
 
@@ -188,25 +197,25 @@ const updateOverallScale = () => {
 }
 
 const setOverallScale = (relativeScalefactor) => {
-  if(relativeScalefactor > 0) {
+  if (relativeScalefactor > 0) {
     document.getElementById("zoomScaleAdd").disabled = zoomScale > 1.0;
     document.getElementById("zoomScaleRemove").disabled = false;
 
-    if(zoomScale < 1.2) {
+    if (zoomScale < 1.2) {
       zoomScale += relativeScalefactor;
       updateOverallScale();
     }
-  }else {
+  } else {
     document.getElementById("zoomScaleRemove").disabled = zoomScale < 0.7;
     document.getElementById("zoomScaleAdd").disabled = false;
 
-    if(zoomScale > 0.5) {
+    if (zoomScale > 0.5) {
       zoomScale += relativeScalefactor;
       updateOverallScale();
     }
   }
 
-  document.getElementById('zoomScaleLabel').innerHTML = `${Math.round(zoomScale*100)}%`
+  document.getElementById('zoomScaleLabel').innerHTML = `${Math.round(zoomScale * 100)}%`
 }
 
 function windowResized() {
@@ -214,51 +223,63 @@ function windowResized() {
 }
 
 function mousePressed() {
-  if (
-    mouseX > posX - rectWidth / 2 &&
-    mouseX < posX + rectWidth / 2 &&
-    mouseY > posY - rectHeight / 2 &&
-    mouseY < posY + rectHeight / 2
-  ) {
+  let rectX = Math.round(posX - (rectWidth / 2));
+  let rectY = Math.round(posY - (rectHeight / 2));
+
+  if (mouseX > rectX && mouseX < rectX + rectWidth && mouseY > rectY && mouseY < rectY + rectHeight) {
     move = true;
 
     offsetX = mouseX - posX;
     offsetY = mouseY - posY;
+
+  } else if (mouseX > rectX && mouseX < rectX + rectWidth) {
+    if (mouseY > rectY - 2 && mouseY < rectY) {
+      resizeGrab = { ...resizeGrab, top: true, startPos: mouseY }
+    } else if (mouseY > rectY + rectHeight && mouseY < rectY + rectHeight + 2) {
+      resizeGrab = { ...resizeGrab, bottom: true, startPos: mouseY }
+    }
+  } else if (mouseY > rectY && mouseY < rectY + rectHeight) {
+    if (mouseX > rectX - 2 && mouseX < rectX) {
+      resizeGrab = { ...resizeGrab, left: true, startPos: mouseX }
+    } else if (mouseX > rectX + rectWidth && mouseX < rectX + rectWidth + 2) {
+      resizeGrab = { ...resizeGrab, right: true, startPos: mouseX }
+    }
   }
 }
 
 
 function mouseReleased() {
   move = false;
+  resizeGrab = { top: false, bottom: false, left: false, right: false, startPos: undefined, scale: 1 }
 }
 
 function mouseWheel(event) {
   if (originalImg) {
-    if(mouseX > 0 && mouseX <= canvasWidth && mouseY > 0 && mouseY <= (originalImg.height / originalImg.width * canvasWidth)){
+    if (mouseX > 0 && mouseX <= canvasWidth && mouseY > 0 && mouseY <= (originalImg.height / originalImg.width * canvasWidth)) {
       if (event.delta < 0) {
-        if(maskScale > 1.0)
+        if (maskScale > 1.0)
           maskScale -= 0.05;
       } else {
-        if(maskScaleStop) return;
-    
-        if(maskScale < 8)
+        if (maskScaleStop) return;
+
+        if (maskScale < 20)
           maskScale += 0.05;
       }
-    
+
       console.log(maskScale);
 
       return false;
-    }  
+    }
   }
 }
 
 function handleImage(file) {
-console.log(file)
+  console.log(file)
 
   if (file.type === 'image') {
     imageLoading = true;
 
-    imageName = file.name.replace(".jpeg", "").replace(".png","").replace(".jpg", "") + " - Skaliert.jpg";
+    imageName = file.name.replace(".jpeg", "").replace(".png", "").replace(".jpg", "") + " - Skaliert.jpg";
 
     document.getElementById("zoomScaleAdd").disabled = true;
     document.getElementById("zoomScaleRemove").disabled = true;
@@ -286,36 +307,36 @@ function handleImageLoaded() {
   document.querySelectorAll('.mdc-radio__native-control').forEach((radio) => radio.disabled = false);
   document.querySelectorAll('.mdc-checkbox__native-control').forEach((radio) => radio.disabled = false);
 
-  posX = canvasWidth/2
-  posY = (originalImg.height / originalImg.width * canvasWidth)/2
+  posX = canvasWidth / 2
+  posY = (originalImg.height / originalImg.width * canvasWidth) / 2
   maskScale = 1
 }
 
 const downloadFile = () => {
   let currentBlob;
-    
-  var factor = originalImg.width/canvasWidth;
-  croppedImage = originalImg.get((posX - rectWidth / 2)*factor, (posY - rectHeight / 2)*factor, rectWidth*factor, rectHeight*factor);
 
-  if(limitExport) {
+  var factor = originalImg.width / canvasWidth;
+  croppedImage = originalImg.get((posX - rectWidth / 2) * factor, (posY - rectHeight / 2) * factor, rectWidth * factor, rectHeight * factor);
 
-  for(var i = 0; i <=5; i++) {
-    console.log("Iteration Count: " + i)
+  if (limitExport) {
 
-    const dataUrl = croppedImage.canvas.toDataURL("image/jpeg", QUALITY);
-    currentBlob = dataURItoBlob(dataUrl);
-    console.log(`Blob-Größe: ${currentBlob.size} Bytes`);
+    for (var i = 0; i <= 5; i++) {
+      console.log("Iteration Count: " + i)
 
-    if (currentBlob.size > MAX_FILE_SIZE) {
-      resizeImage(currentBlob.size, croppedImage);
-    }else {
-      console.log("Finished")
-      console.log(currentBlob.size)
-      break;
+      const dataUrl = croppedImage.canvas.toDataURL("image/jpeg", QUALITY);
+      currentBlob = dataURItoBlob(dataUrl);
+      console.log(`Blob-Größe: ${currentBlob.size} Bytes`);
+
+      if (currentBlob.size > MAX_FILE_SIZE) {
+        resizeImage(currentBlob.size, croppedImage);
+      } else {
+        console.log("Finished")
+        console.log(currentBlob.size)
+        break;
+      }
     }
-  }
 
-  }else {
+  } else {
     const dataUrl = croppedImage.canvas.toDataURL("image/jpeg", QUALITY);
     currentBlob = dataURItoBlob(dataUrl);
     console.log(`Blob-Größe: ${currentBlob.size} Bytes`);
@@ -362,9 +383,9 @@ const dataURItoBlob = (dataURI) => {
   var ab = new ArrayBuffer(byteString.length);
   var ia = new Uint8Array(ab);
   for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
+    ia[i] = byteString.charCodeAt(i);
   }
-  var blob = new Blob([ab], {type: mimeString});
+  var blob = new Blob([ab], { type: mimeString });
   return blob;
 }
 
@@ -372,7 +393,7 @@ const saveBlob = (blob, fileName) => {
   var a = document.createElement("a");
   document.body.appendChild(a);
   a.style = "display: none";
-  
+
   var url = URL.createObjectURL(blob);
   a.href = url;
   a.download = fileName;
